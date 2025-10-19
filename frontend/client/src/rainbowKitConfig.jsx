c326d09f4c82ea25f6dd9ce1499ae7e93c0000cc
OrsiniBr <udochukwugodwin13@gmail.com>
2025-10-19 04:23:06 +0100

    add custom rpc url

--- frontend/client/src/rainbowKitConfig.jsx
+++ frontend/client/src/rainbowKitConfig.jsx
@@ -2,10 +2,14 @@
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { polygonAmoy } from "wagmi/chains";
+import { http } from "wagmi";
 
 export default getDefaultConfig({
   appName: "Cross-Credit Lending",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
   chains: [polygonAmoy],
+  transports: {
+    [polygonAmoy.id]: http(import.meta.env.VITE_ALCHEMY_RPC_URL),
+  },
   ssr: false,
 });
