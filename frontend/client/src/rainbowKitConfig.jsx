2fc82d52e33348c7b3d68923a940dd1800dfaabf
OrsiniBr <udochukwugodwin13@gmail.com>
2025-11-28 20:33:46 +0100

    change to mainnet

--- frontend/client/src/rainbowKitConfig.jsx
+++ frontend/client/src/rainbowKitConfig.jsx
@@ -1,19 +1,18 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { polygonAmoy } from "wagmi/chains";
+import { polygon} from "wagmi/chains";
 import { http } from "wagmi";
 
 export default getDefaultConfig({
   appName: "Cross-Credit Lending",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
-  chains: [polygonAmoy],
-  transports: {
-    [polygonAmoy.id]: [
-      http(import.meta.env.VITE_ALCHEMY_RPC_URL),
-      http(import.meta.env.VITE_INFURA_RPC_URL),
-    ],
-  },
+  chains: [polygon],
+  // transports: {
+  //   [polygonAmoy.id]: [
+  //     http(import.meta.env.VITE_ALCHEMY_RPC_URL),
+  //     http(import.meta.env.VITE_INFURA_RPC_URL),
+  //   ],
+  // },
   ssr: false,
 });
- 
